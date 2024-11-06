90b9eab64fc9b3a22e2f890be8d6086bb659f165
Suze <suze.schrama@gmail.com>
2024-11-06 12:42:24 +0100

    made some changes to package.json which resulted in changes in output.css and package-lock.json

---
+++ src/main/java/com/tamingthymeleaf/taming_thymeleaf/user/web/UserController.java
@@ -0,0 +1,15 @@
+package com.tamingthymeleaf.taming_thymeleaf.user.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String index(Model model) {
+        return "users/list";
+    }
+}
